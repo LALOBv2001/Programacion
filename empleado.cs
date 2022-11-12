ba34042a2f1fbe18c99190b9ee5ff65ef9a7a6dd
LALOBv2001 <79590204+LALOBv2001@users.noreply.github.com>
2022-11-12 15:50:17 -0600

    a

---
+++ empleado.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaGenericaDeEmpleados
+{
+    class Empleado 
+    {
+		private int _intNumero;
+
+		public int Numero
+		{
+			get { return _intNumero; }
+			set { _intNumero = value; }
+		}
+		private string _strNombre;
+
+		public string Nombre
+		{
+			get { return _strNombre; }
+			set { _strNombre = value; }
+		}
+		private double  _dblSueldo;
+
+		public double  Sueldo 
+		{
+			get { return _dblSueldo; }
+			set { _dblSueldo = value; }
+		}
+
+
+
+	}
+}
